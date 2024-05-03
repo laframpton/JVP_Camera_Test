f0f7039192c2f6f9b310ed99ac9c2e64c1988b6c
laframpton <laframpton@outlook.com>
2024-05-03 16:42:41 -0230

    feature: capture test now captures and records temperatures, returned camera_capture to default for reference

---
+++ capture_test.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from pypylon import pylon
+
+camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
+print(camera)
+
+camera.Open()
+camera.TriggerSource.Value = "Software"
+
+camera.StartGrabbingMax(300)
+
+images = []
+grabbing_details = []
+
+camera.DeviceTemperatureSelector.Value = "FpgaCore"
+print('Capturing the temperature at: ' + camera.DeviceTemperatureSelector.Value)
+
+while camera.IsGrabbing():
+    grab_result = camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
+
+    if grab_result.GrabSucceeded():
+        images.append(grab_result.Array)
+        grabbing_details.append((grab_result.TimeStamp / 1e9, camera.DeviceTemperature.Value))
+
+    grab_result.Release()
+
+camera.Close()
+
+grabbing_details = pd.DataFrame(grabbing_details, columns=['Time Stamp', 'Temperature'])
+
+plt.plot(grabbing_details['Temperature'], label='Temperature @ ' + "FpgaCore", color='blue')
+plt.axhline(85, label="Critical Temperature Threshold", color='orange')
+plt.legend()
+plt.ylim([30, 100])
+
+print(grabbing_details)
--- test.py
+++
@@ -1,5 +0,0 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-from pypylon import pylon
-
-camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
