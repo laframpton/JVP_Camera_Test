fe47102978df31eb8c63facc37f76b83ed541577
laframpton <laframpton@outlook.com>
2024-05-06 12:34:32 -0230

    feature: array of average pixel intensities

--- capture_test.py
+++ capture_test.py
@@ -3,13 +3,16 @@ import pandas as pd
 from pypylon import pylon
 import numpy as np
 
+MAX_FRAMES = 300
+EXPOSURE_TIME = 20000
+
 camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
 print(camera)
 
 camera.Open()
 camera.TriggerSource.Value = "Software" # This sets the camera to work soley off of this software
 
-camera.StartGrabbingMax(300)
+camera.StartGrabbingMax(MAX_FRAMES)
 
 images = []
 grabbing_details = []
@@ -18,7 +21,7 @@ camera.DeviceTemperatureSelector.Value = "FpgaCore"
 print('Capturing the temperature at: ' + camera.DeviceTemperatureSelector.Value)
 
 while camera.IsGrabbing():
-    grab_result = camera.RetrieveResult(20000, pylon.TimeoutHandling_ThrowException)
+    grab_result = camera.RetrieveResult(EXPOSURE_TIME, pylon.TimeoutHandling_ThrowException)
 
     if grab_result.GrabSucceeded():
         images.append(grab_result.Array)
@@ -40,3 +43,10 @@ print(grabbing_details)
 ### For testing
 print(pd.DataFrame(images[0]).describe())
 #pd.DataFrame(images[1]).to_csv('imagesdf.csv')
+
+avg_intensity = np.empty((0,0))
+for frame in range(MAX_FRAMES):
+    mean = np.array(images[frame]).mean()
+    avg_intensity = np.append(avg_intensity,mean)
+
+print(avg_intensity)
